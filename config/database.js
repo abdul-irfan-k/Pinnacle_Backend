f270a479048dfe6fbd994c0c29bd95d5f474d6a4
irfan7k7 <unknown7k7k@gmail.com>
2023-05-29 07:26:52 +0530

    modified

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async (callBackFunc) => {
     try {   
-        const connect = await mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/Pinnacle')
+        const connect = await mongoose.connect('mongodb+srv://spcpinnacle2k23:xVqT1RokNB8TUs3A@cluster0.ns3fqql.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/Pinnacle')
         callBackFunc()
     } catch (error) {
         console.log(error)
